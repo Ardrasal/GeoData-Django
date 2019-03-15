447d1c7decf6a2fd5474b3040616571f42b71ac9
Ardra Salemy <ardrasalemy@gmail.com>
2019-03-15 10:48:26 -0400

    Cleans notes in load_geodata.py.

---
+++ heatmap/management/commands/load_geodata.py
@@ -0,0 +1,25 @@
+# I used this tutorial to remind me how to make a management command to load the parsed csv data: https://simpleisbetterthancomplex.com/tutorial/2018/08/27/how-to-create-custom-django-management-commands.html
+
+# Called with 'python manage.py load_geodata'
+
+from django.core.management.base import BaseCommand
+
+# To extract the lat/long data, I used this answer "With pandas you can use read_csv with usecols parameter" from https://stackoverflow.com/questions/16503560/read-specific-columns-from-a-csv-file-with-csv-module/41004218#41004218
+
+import pandas as pd
+# import io  (used in truncated-data test)
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        # Truncated_data test:
+        # truncated_data = '''
+        # network,geoname_id,registered_country_geoname_id,represented_country_geoname_id,is_anonymous_proxy,is_satellite_provider,postal_code,latitude,longitude,accuracy_radius
+        # 1.0.0.0/24,2070667,2077456,,0,0,5214,-35.5016,138.7819,100
+        # 1.0.1.0/24,1811017,1814991,,0,0,,24.4798,118.0819,50
+        # '''
+        # df = pd.read_csv(io.StringIO(truncated_data), usecols=["latitude", "longitude"])
+        # Parses from entire csv file:
+        df = pd.read_csv(("heatmap/.GeoLite2-City-CSV_20190312/.GeoLite2-City-Blocks-IPv4.csv"), usecols=["latitude", "longitude"])
+        # saved_lat = df.latitude
+        print(df)

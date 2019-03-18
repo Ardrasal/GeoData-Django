055eca664c7d970d0c82bf073309fd62a3cbb668
Ardra Salemy <ardrasalemy@gmail.com>
2019-03-18 16:40:10 -0400

    Adjusts models and starts to switch from pandas to csvreader.

--- heatmap/data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import requests
-import json
-from pandas.io.json import json_normalize
-
-df = pd.read_csv(("heatmap/.GeoLite2-City-CSV_20190312/.GeoLite2-City-Blocks-IPv4.csv"), usecols=["latitude", "longitude"])
-df = df[['latitude', 'longitude']]
-df.head()

dacff318f8d0bf4b0025382f03c61f3b82403c25
Ardra Salemy <ardrasalemy@gmail.com>
2019-03-18 22:20:14 -0400

    Fixes management command to create LatLong model objects.

--- heatmap/management/commands/load_geodata.py
+++ heatmap/management/commands/load_geodata.py
@@ -1,26 +1,25 @@
 # I used this tutorial to remind me how to make a management command to load the parsed csv data: https://simpleisbetterthancomplex.com/tutorial/2018/08/27/how-to-create-custom-django-management-commands.html
 
+# Adapted code from https://stackoverflow.com/questions/16503560/read-specific-columns-from-a-csv-file-with-csv-module
+
 # Called with 'python manage.py load_geodata'
 
 from django.core.management.base import BaseCommand
 import csv
 from heatmap.models import LatLong
 
-import pandas as pd
-
 class Command(BaseCommand):
-    # df = pd.read_csv(("heatmap/.GeoLite2-City-CSV_20190312/.GeoLite2-City-Blocks-IPv4.csv"), usecols=["latitude", "longitude"])
-    
-    with open('heatmap/.GeoLite2-City-CSV_20190312/.GeoLite2-City-Blocks-IPv4.csv') as csvfile:
-        csvreader = csv.reader(csvfile)
-        
-        next(csvreader)
-        for row in df:
-            # breakpoint()
+    # ("heatmap/.GeoLite2-City-CSV_20190312/.GeoLite2-City-Blocks-IPv4.csv")
+    def handle(self, *args, **kwargs):
+        with open('heatmap/.GeoLite2-City-CSV_20190312/.GeoLite2-City-Blocks-IPv4.csv') as csvfile:
 
-    with open('heatmap/testdata.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
-        next(csvreader)
+            # get number of columns
+            # num_columns = len(csvfile.readline().split(','))
+            len(csvfile.readline().split(','))
+            csvfile.seek(0)
+
+            reader = csv.DictReader(csvfile)
             for row in reader:
-            #  clean all this up and add specific columns
-            LatLong.objects.create(latitude=row['latitude'], longitude=row['longitude'])
+                print(row['latitude'])
+                LatLong.objects.create(
+                    latitude=row['latitude'], longitude=row['longitude'])

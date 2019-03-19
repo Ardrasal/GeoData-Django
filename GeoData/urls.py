40cc3995ba119e0a68ccdc9e072d139adce512ce
Ardra Salemy <ardrasalemy@gmail.com>
2019-03-19 15:39:28 -0400

    Updates default rest_framework setting to keep integrity of decimal field in settings, and rearranges urls, following api book tutorial.

--- GeoData/urls.py
+++ GeoData/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from heatmap import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
     path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
     path('api/', include('api.urls')),
 ]

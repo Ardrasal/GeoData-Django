a36b584588122ac396cfdccce9ba425317a14901
Ardra Salemy <ardrasalemy@gmail.com>
2019-03-19 20:40:58 -0400

    Minor code styling adjustments before updating heroku.

--- api/views.py
+++ api/views.py
@@ -1,21 +1,23 @@
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# from rest_framework.response import Response
 from rest_framework import generics
 from heatmap.models import LatLong
 from .serializers import LatLongSerializer
-from django.http import HttpResponse, JsonResponse
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from django.http import JsonResponse
+# from rest_framework.renderers import JSONRenderer
+# from rest_framework.parsers import JSONParser
 
-# creates a read only endpoint for all LatLong instances
 class LatLongAPIView(generics.ListAPIView):
+    """
+    Creates a read only endpoint for all LatLong instances.
+    """
     queryset = LatLong.objects.all()
     serializer_class = LatLongSerializer
 
 @api_view(['GET'])
 def latlong_list(request):
     """
-    List all latlong objects.
+    Lists all latlong objects.
     """
     if request.method == 'GET':
         latlongs = LatLong.objects.all()
